f624e94d79d436494747a6c967319aaec48d6c76
Luis Alejandro Gallego Ching <lordchingzo@gmail.com>
2024-04-01 14:18:57 -0500

    agrega funcionalidad para delete_pattern que permite que la ruta de updateDataByClient pueda borrar patrones al agregar delete_pattern en true

--- src/services/dataService.js
+++ src/services/dataService.js
@@ -11,20 +11,38 @@ export async function getDataByCliente(cliente) {
 
 
 export async function updateDataByCliente(cliente, newData) {
-    const { patterns_auto_admisorio, patterns_escrito_tutela } = newData;
+    const { patterns_auto_admisorio, patterns_escrito_tutela, delete_pattern } = newData;
 
     const updates = {};
 
-    if (patterns_auto_admisorio && patterns_auto_admisorio.length > 0) {
-        updates['$push'] = { patterns_auto_admisorio: { $each: patterns_auto_admisorio } };
+    if (delete_pattern) {
+        if (patterns_auto_admisorio) {
+            if (Array.isArray(patterns_auto_admisorio)) {
+                updates['$pull'] = { patterns_auto_admisorio: { $in: patterns_auto_admisorio } };
+            } else {
+                updates['$pull'] = { patterns_auto_admisorio: patterns_auto_admisorio };
+            }
+        }
+
+        if (patterns_escrito_tutela) {
+            if (Array.isArray(patterns_escrito_tutela)) {
+                updates['$pull'] = updates['$pull'] || {};
+                updates['$pull'].patterns_escrito_tutela = { $in: patterns_escrito_tutela };
+            } else {
+                updates['$pull'] = updates['$pull'] || {};
+                updates['$pull'].patterns_escrito_tutela = patterns_escrito_tutela;
+            }
+        }
+    } else {
+        if (patterns_auto_admisorio && patterns_auto_admisorio.length > 0) {
+            updates['$push'] = { patterns_auto_admisorio: { $each: patterns_auto_admisorio } };
+        }
+
+        if (patterns_escrito_tutela && patterns_escrito_tutela.length > 0) {
+            updates['$push'] = updates['$push'] || {};
+            updates['$push'].patterns_escrito_tutela = { $each: patterns_escrito_tutela };
+        }
     }
-
-    if (patterns_escrito_tutela && patterns_escrito_tutela.length > 0) {
-
-        updates['$push'] = updates['$push'] || {};
-        updates['$push'].patterns_escrito_tutela = { $each: patterns_escrito_tutela };
-    }
-
 
     return await DataModel.findOneAndUpdate({ cliente }, updates, { new: true });
 }
